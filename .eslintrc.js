6ae44d7bd3515ca068b09684e9aeedd0fd3e8666
Osman Zakir <osmanzakir90@hotmail.com>
2020-10-12 03:38:04 +0500

    added eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2020,
+    "parser": "babel",
+    "sourceType": "script"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "yoda": "error",
+    "eqeqeq": "error",
+    "prefer-const": "error",
+    "no-var": "error",
+    "space-before-function-paren": "off"
+  }
+};
