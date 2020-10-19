63bb88007e09e9dc0fb7c1bdd61698a017555f0d
Osman Zakir <osmanzakir90@hotmail.com>
2020-10-19 23:52:53 +0500

    made some changes to JS code, got some more tests to pass, and installed eslint jquery config

--- scripts/script.js
+++ scripts/script.js
@@ -1,125 +1,171 @@
 "use strict";
 
+let isSessionRunning = true;
+const timerElem = $("#time-left");
+let breakIncremented = false;
+let breakDecremented = false;
+let sessionIncremented = false;
+let sessionDecremented = false;
+
 const breakDecrementButton = $("#break-decrement");
-try {
-  breakDecrementButton.on("click", () => {
-    const breakLength = $("#break-length");
-    let lengthValue = Number(breakLength.text());
-    if (lengthValue > 1) {
-      lengthValue--;
-    } else if (lengthValue < 1) {
-      throw "lengthValue must not be less than 1";
-    }
-    breakLength.text(lengthValue);
-  });
-} catch (err) {
-  console.log(`Error: ${err}`);
-}
+breakDecrementButton.on("click", () => {
+  breakDecremented = true;
+  const breakLength = $("#break-length");
+  let lengthValue = Number(breakLength.text());
+  if (lengthValue > 1) {
+    lengthValue--;
+  }
+  breakLength.text(lengthValue);
+  if (!isSessionRunning) {
+    timerElem.text(`${breakLength.text()}:00`);
+  }
+});
 
 const breakIncrementButton = $("#break-increment");
-try {
-  breakIncrementButton.on("click", () => {
-    const breakLength = $("#break-length");
-    let lengthValue = Number(breakLength.text());
-    if (lengthValue < 60) {
-      lengthValue++;
-    } else if (lengthValue === 60) {
-      throw "lengthValue must be greater than 60";
-    }
-    breakLength.text(lengthValue);
-  });
-} catch (err) {
-  console.log(`Error: ${err}`);
-}
+breakIncrementButton.on("click", () => {
+  breakIncremented = true;
+  const breakLength = $("#break-length");
+  let lengthValue = Number(breakLength.text());
+  if (lengthValue < 60) {
+    lengthValue++;
+  } else if (lengthValue === 60) {
+    // do nothing to the value
+    lengthValue;
+  }
+  breakLength.text(lengthValue);
+  if (!isSessionRunning) {
+    timerElem.text(`${breakLength.text()}:00`);
+  }
+});
 
 const sessionDecrementButton = $("#session-decrement");
-try {
-  sessionDecrementButton.on("click", () => {
-    const sessionLength = $("#session-length");
-    let lengthValue = Number(sessionLength.text());
-    if (lengthValue > 1) {
-      lengthValue--;
-    } else if (lengthValue < 1) {
-      throw "lengthValue must not be less than 1";
-    }
-    sessionLength.text(lengthValue);
-  });
-} catch (err) {
-  console.log(`Error: ${err}`);
-}
+sessionDecrementButton.on("click", () => {
+  sessionDecremented = true;
+  const sessionLength = $("#session-length");
+  let lengthValue = Number(sessionLength.text());
+  if (lengthValue > 1) {
+    lengthValue--;
+  }
+  sessionLength.text(lengthValue);
+  if (isSessionRunning) {
+    timerElem.text(`${sessionLength.text()}:00`);
+  }
+});
 
 const sessionIncrementButton = $("#session-increment");
-try {
-  sessionIncrementButton.on("click", () => {
-    const sessionLength = $("#session-length");
-    let lengthValue = Number(sessionLength.text());
+sessionIncrementButton.on("click", () => {
+  sessionIncremented = true;
+  const sessionLength = $("#session-length");
+  let lengthValue = Number(sessionLength.text());
+  if (lengthValue < 60) {
     lengthValue++;
-    sessionLength.text(lengthValue);
-  });
-} catch (err) {
-  console.log(`Error: ${err}`);
-}
-
-const defaultSessionLength = 25;
-const defaultBreakLength = 5;
-
-// default session and break length in seconds
-const sessionSeconds = defaultSessionLength * 60;
-const breakSeconds = defaultBreakLength * 60;
-
-let isSessionRunning = true;
-let isTimerRunning = false;
-
-const timerElem = $("#time-left");
-let timeRemaining = Number($("#time-left").text());
-
-const timerCallback = () => {
-  let timer = (isSessionRunning) ? sessionSeconds : breakSeconds;
-
-  // timer value should be 0 at first, so add 1 and then multiply
-  if ($("#timer-label").text() === "Session") {
-    timer += 1;
-    timer *= defaultSessionLength;
-    isSessionRunning = true;
-  } else if ($("#timer-label").text === "Break") {
-    timer += 1;
-    timer *= defaultBreakLength;
-    isSessionRunning = false;
+  } else if (lengthValue === 60) {
+    // do nothing to the value
+    lengthValue;
   }
-
-  const minutes = parseInt(timer) / 60;
-  const seconds = parseInt(timer) % 60;
-  if (minutes < 10) {
-    minutes.padStart(2, "0");
+  sessionLength.text(lengthValue);
+  if (isSessionRunning) {
+    timerElem.text(`${sessionLength.text()}:00`);
   }
+});
 
-  if (seconds < 10) {
-    seconds.padStart(2, "0");
-  }
+const addLeadingZeroes = time => {
+  return time < 10 ? `0${time}` : time;
+};
 
-  timeRemaining = `${minutes}:${seconds}`;
-  timerElem.text(timeRemaining);
-  isTimerRunning = true;
+let today = new Date();
+let deadline = new Date();
+let deadlineMins = ((isSessionRunning) ? 
+  Number($("#session-length").text()) : 
+  Number($("#break-length").text())
+);
+deadline.setMinutes(today.getMinutes() + deadlineMins);
+let total = Date.parse(deadline.toString()) - Date.parse(today.toString());
+let secondsRemaining = total / 1000;
 
-  if (minutes === 0 && seconds === 0) {
-    if ($("#timer-label").text() === "Session") {
-      isSessionRunning = false;
-    } else if ($("#timer-label").text() === "Break") {
-      isSessionRunning = true;
-    }
-    
-    const audioElem = $("#beep");
-    audioElem.play();
+let isClockRunning = false;
+let timerTextContent = timerElem.text();
+let timerId;
+const initializeClock = () => {  
+  if (!isClockRunning) {
+    isClockRunning = true;
+    timerId = setInterval(() => {
+      if (secondsRemaining <= 0) {
+        clearInterval(timerId);
+        const audioElem = $("#beep");
+        audioElem.play();
+        if ($("#timer-label").text() === "Session") {
+          isSessionRunning = false;
+          $("#timer-label").text("Break");
+        } else if ($("#timer-label").text() === "Break") {
+          isSessionRunning = true;
+          $("#timer-label").text("Session");
+        }
+      }
+  
+      if ($("#timer-label").text() === "Session") {
+        isSessionRunning = true;
+      } else if ($("#timer-label").text === "Break") {
+        isSessionRunning = false;
+      }
+  
+      timerElem.text(`${((isSessionRunning) ? 
+        Number($("#session-length").text()) : 
+        Number($("#break-length").text())
+      )}:00`);
+  
+      if (sessionIncremented || sessionDecremented || breakIncremented || breakDecremented) {
+        // reset secondsRemaining
+        today = new Date();
+        deadline = new Date();
+        deadlineMins = ((isSessionRunning) ? 
+          Number($("#session-length").text()) : 
+          Number($("#break-length").text())
+        );
+        deadline.setMinutes(today.getMinutes() + deadlineMins);
+        total = Date.parse(deadline.toString()) - Date.parse(today.toString());
+        secondsRemaining = total / 1000;
+      }
+
+      const minutes = parseInt(secondsRemaining.toString() / 60);
+      const seconds = parseInt(secondsRemaining.toString() % 60);
+
+      secondsRemaining--;
+      timerTextContent = `${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`;
+      timerElem.text(timerTextContent);
+      console.log(secondsRemaining);
+      isClockRunning = true;
+    }, 1000);
+  } else {
+    clearInterval(timerId);
+    isClockRunning = false;
   }
 };
 
-const timerId = setInterval(timerCallback, 1000);
-
 const startStopButton = $("#start_stop");
-startStopButton.on("click", () => {
-  if (isTimerRunning) {
-    clearInterval(timerId);
-  } else {
-    setInterval(timerCallback, 1000);
-  }
+startStopButton.on("click", initializeClock);
+
+const resetButton = $("#reset");
+resetButton.on("click", () => {
+  // reset app state to default
+  clearInterval(timerId);
+  const sessionLengthElem = $("#session-length");
+  sessionLengthElem.text("25");
+  
+  const breakLengthElem = $("#break-length");
+  breakLengthElem.text("5");
+  
+  $("#timer-label").text("Session");
+
+  today = new Date();
+  deadline = new Date();
+  deadline.setMinutes(today.getMinutes() + 25);
+  total = Date.parse(deadline.toString()) - Date.parse(today.toString());
+  secondsRemaining = total / 1000;
+  
+  const minutes = parseInt(secondsRemaining.toString() / 60);
+  const seconds = parseInt(secondsRemaining.toString() % 60);
+
+  timerTextContent = `${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`;
+  timerElem.text(timerTextContent);
 });
